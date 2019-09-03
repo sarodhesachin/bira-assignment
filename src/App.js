fe76772e58e6ca07702fa7acf902a491b7d2bc15
sarodhesachin <sachin.s@hashedin.com>
2019-09-03 17:40:25 +0530

    added header, sidebar and dashboard component
    Signed-off-by: sarodhesachin <sachin.s@hashedin.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Dashboard from './dashboard/Dashboard';
+import Header from './header/Header'
+import Sidebar from './sidbar/Sidebar'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Sidebar/>
+      <Dashboard/>
     </div>
   );
 }
---
+++ src/sidbar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Sidebar.css'
+
+class Sidebar extends React.Component {
+	render() {
+		return (
+			<div className='Sidebar'>
+				Sidebar
+			</div>
+		)
+	}
+}
+
+export default Sidebar;
